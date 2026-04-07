6d72b37de61a7f35f753befb4968ade63db4a6cb
HeinrichAD <HeinrichAD@users.noreply.github.com>
2026-04-07 13:08:34 +0200

    switch file header order

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
+/// <reference types="vite/client" />
+
 // SPDX-FileCopyrightText: 2026 German Aerospace Center (DLR)
 // SPDX-License-Identifier: Apache-2.0
-
-/// <reference types="vite/client" />
